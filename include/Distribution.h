dd213a0c31b9092ae7a45175fa869e6f75afdda8
SamG-01 <sam1glosser@gmail.com>
2024-04-15 12:04:38 -0400

    add arithmetic operator overloads

--- include/Distribution.h
+++ include/Distribution.h
@@ -55,10 +55,143 @@ class Distribution {
         T standard_deviation() const {return _standard_deviation; }
         T std_dev() const {return _standard_deviation; }
 
-        // TODO: cout, comparisons (compare means)
-        // TODO: consider alternate implementation with Eigen package
+        /// @brief Methods
 
+        /// @brief Applies a function to `samples` element-wise.
+        /// @param func the function to be applied.
+        /// @return The resulting distribution.
+        Distribution<T> apply(T func(T)) {
+            new_samples = this->samples.apply(func);
+            return Distribution<T>(new_samples);
+        }
 
+        /// @brief `Distribution`-`Distribution` operations
+
+        /// @brief Add `Distribution`s by adding their samples element-wise.
+        /// @param Dist2 second summand.
+        /// @return The resulting distribution.
+        Distribution<T> operator+ (const Distribution<T> Dist2) {
+            new_samples = _samples + Dist2.samples();
+            return Distribution<T>(new_samples)
+        }
+
+        /// @brief Add `Distribution`s by adding their samples element-wise.
+        /// @param Dist2 second summand.
+        /// @return The resulting distribution.
+        Distribution<T> operator+ (const Distribution<T> Dist2) {
+            new_samples = _samples + Dist2.samples();
+            return Distribution<T>(new_samples)
+        }
+        
+        /// @brief Subtract `Distribution`s by subtracting their samples element-wise.
+        /// @param Dist2 the subtrahend.
+        /// @return The resulting distribution.
+        Distribution<T> operator- (const Distribution<T> Dist2) {
+            new_samples = _samples - Dist2.samples();
+            return Distribution<T>(new_samples)
+        }
+        
+        /// @brief Multiply `Distribution`s by multiplying their samples element-wise.
+        /// @param Dist2 the multiplier.
+        /// @return The resulting distribution.
+        Distribution<T> operator* (const Distribution<T> Dist2) {
+            new_samples = _samples * Dist2.samples();
+            return Distribution<T>(new_samples)
+        }
+
+        /// @brief Divide `Distribution`s by dividing their samples element-wise.
+        /// @param Dist2 the divisor.
+        /// @return The resulting distribution.
+        Distribution<T> operator/ (const Distribution<T> Dist2) {
+            new_samples = _samples / Dist2.samples();
+            return Distribution<T>(new_samples)
+        }
+
+        /// @brief `Distribution`-scalar operations.
+
+        /// @brief Sum of a `Distribution` and a scalar.
+        /// @param scalar summand of type `T`.
+        /// @return The resulting distribution.
+        Distribution<T> operator* (const T scalar) {
+            return Distribution<T>(_samples + scalar);
+        }
+
+        /// @brief Subtract a `Distribution` by a scalar.
+        /// @param scalar subtrahend of type `T`.
+        /// @return The resulting distribution.
+        Distribution<T> operator* (const T scalar) {
+            return Distribution<T>(_samples - scalar);
+        }
+
+        /// @brief Multiply a `Distribution` by a scalar.
+        /// @param scalar multiplier of type `T`.
+        /// @return The resulting distribution.
+        Distribution<T> operator* (const T scalar) {
+            return Distribution<T>(_samples * scalar);
+        }
+        
+        /// @brief Divide a `Distribution` by a scalar.
+        /// @param scalar divisor of type `T`.
+        /// @return The resulting distribution.
+        Distribution<T> operator/ (const T scalar) {
+            return Distribution<T>(_samples / scalar);
+        }
+
+        /// @brief Unary minus.
+        /// @return The negated `Distribution`.
+        Distribution<T> operator- () {
+            return Distribution<T>(-1 * _samples);
+        }
+
+        /// @brief Friends
+        friend Distribution<T> operator+ (T, const Distribution<T>);
+        friend Distribution<T> operator- (T, const Distribution<T>);
+        friend Distribution<T> operator* (T, const Distribution<T>);
+        friend Distribution<T> operator/ (T, const Distribution<T>);
 };
+
+/// @brief Friends
+
+/// @brief Scalar-`Distribution` operations.
+
+/// @brief Add a `Distribution` to a scalar.
+/// @tparam T the data type of the scalar and `Distribution`.
+/// @param scalar the scalar of type `T` to multiply by.
+/// @param Dist The `Distribution` to multiply by.
+/// @return The resulting `Distribution`.
+template <typename T>
+Distribution<T> operator+ (T scalar, const Distribution<T>& Dist) {
+    return Distribution<T>(scalar + Dist.samples())
+}
+
+/// @brief Subtract a scalar by a `Distribution`.
+/// @tparam T the data type of the scalar and `Distribution`.
+/// @param scalar the minuend of type `T`.
+/// @param Dist The `Distribution` to subtract.
+/// @return The resulting `Distribution`.
+template <typename T>
+Distribution<T> operator- (T scalar, const Distribution<T>& Dist) {
+    return Distribution<T>(scalar - Dist.samples())
+}
+
+/// @brief Multiply a scalar by a `Distribution`.
+/// @tparam T the data type of the scalar and  the samples in`Distribution`.
+/// @param scalar the multiplicand of type `T`.
+/// @param Dist The `Distribution` to multiply by.
+/// @return The resulting `Distribution`
+template <typename T>
+Distribution<T> operator* (T scalar, const Distribution<T>& Dist) {
+    return Distribution<T>(scalar * Dist.samples())
+}
+
+/// @brief Divide a scalar by a `Distribution`.
+/// @tparam T the data type of the scalar and  the samples in`Distribution`.
+/// @param scalar the dividend of type `T`.
+/// @param Dist The `Distribution` to divide by.
+/// @return The resulting `Distribution`.
+template <typename T>
+Distribution<T> operator/ (T scalar, const Distribution<T>& Dist) {
+    return Distribution<T>(scalar / Dist.samples())
+}
 
 #endif
