cd3e9a0489421ef766cb80d0837f55780a26460c
SamG-01 <sam1glosser@gmail.com>
2024-04-21 21:51:44 -0400

    add default_rng, seed customization

--- include/RandomDistribution.h
+++ include/RandomDistribution.h
@@ -5,18 +5,19 @@
 
 #include <random>
 
-/// @brief Creates source of pseudo-random numbers.
 std::random_device rd;
-std::mt19937 gen(rd());
+std::mt19937 default_rng(rd());
 
+/// @brief Creates source of pseudo-random numbers.
 /// @brief Helper function to generate `Distribution` objects using sources of random numbers.
 /// @tparam T the data type.
 /// @tparam DT the type of `dis`.
 /// @param dis a random number distribution (see `<random>`).
 /// @param _num_samples the number of samples to generate.
+/// @param gen the random seed (defaults to a random value).
 /// @return A `Distribution` containing the generated data.
 template <typename T, typename DT>
-Distribution<T> RandomDistribution(DT dis, int _num_samples) {
+Distribution<T> RandomDistribution(DT dis, int _num_samples, std::mt19937 gen = default_rng) {
 
     std::valarray<T> _samples(_num_samples);
 
@@ -32,11 +33,12 @@ Distribution<T> RandomDistribution(DT dis, int _num_samples) {
 /// @param mu the mean of the distribution.
 /// @param b the standard deviation of the distribution.
 /// @param _num_samples the number of samples to generate (default 1000).
+/// @param gen the random seed (defaults to a random value).
 /// @return A `Distribution` containing the generated data.
 template <typename T>
-Distribution<T> Normal(T mu, T sigma, int _num_samples = 1000) {
+Distribution<T> Normal(T mu, T sigma, int _num_samples = 1000, std::mt19937 gen = default_rng) {
     std::normal_distribution<> dis(mu, sigma);
-    return RandomDistribution<T, std::normal_distribution<T>>(dis, _num_samples);
+    return RandomDistribution<T, std::normal_distribution<T>>(dis, _num_samples, gen);
 }
 
 /// @brief Creates a uniform `Distribution` of real numbers on the interval [a, b).
@@ -44,11 +46,12 @@ Distribution<T> Normal(T mu, T sigma, int _num_samples = 1000) {
 /// @param a the minimum of the distribution (inclusive).
 /// @param b the maximum of the distribution (exclusive).
 /// @param _num_samples the number of samples to generate (default 1000).
+/// @param gen the random seed (defaults to a random value).
 /// @return A `Distribution` containing the generated data.
 template <typename T>
-Distribution<T> Uniform(T a, T b, int _num_samples = 1000) {
+Distribution<T> Uniform(T a, T b, int _num_samples = 1000, std::mt19937 gen = default_rng) {
     std::uniform_real_distribution<> dis(a, b);
-    return RandomDistribution<T, std::uniform_real_distribution<T>>(dis, _num_samples);
+    return RandomDistribution<T, std::uniform_real_distribution<T>>(dis, _num_samples, gen);
 }
 
 #endif
