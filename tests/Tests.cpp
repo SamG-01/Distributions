c976cb3858669c76cf86f376df7609c807e9a7a9
SamG-01 <sam1glosser@gmail.com>
2024-04-23 00:40:00 -0400

    add assert messages

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -9,14 +9,7 @@
 
 std::mt19937 test_rng(0);
 
-/// @brief Runs a unit test using `assert`.
-/// @param description A brief description of the test.
-/// @param condition The test itself.
-void unit_test(std::string description, bool condition) {
-    std::cout << description << ": ";
-    assert(condition);
-    std::cout << "passed." << std::endl;
-}
+#define assertm(exp, msg) assert(((void)msg, exp))
 
 int main() {
     // Constructors
@@ -24,11 +17,11 @@ int main() {
     Distribution<double> U = Uniform(3.0, 0.25, 1000, test_rng);
 
     // Getters
-    //unit_test("Distribution.samples()", N.samples()[0] == 1);
-    unit_test("Distribution.num_samples()", U.num_samples() == 1000);
-    unit_test("Distribution.mean()", N.mean() == 2.00701);
-    unit_test("Distribution.variance()", U.variance() == 0.0648154);
-    unit_test("Distribution.standard_deviation()", N.standard_deviation() == 1.509177);
+    assertm(N.samples()[0] == 1, "Distribution.samples()");
+    assertm(U.size() == 1000, "Distribution.size()");
+    assertm(N.mean() == 2.00701, "Distribution.mean()");
+    assertm(U.variance() == 0.0648154, "Distribution.variance()");
+    assertm(N.standard_deviation() == 0.509177, "Distribution.standard_deviation()");
 
     return 0;
 }
